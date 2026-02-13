428c26fc54836a0e23a7281b83220c3e264a7c7e
Edward Leon <edleon761@gmail.com>
2026-02-13 14:06:35 -0800

    Testing nav bar
    - seeing if ./ notation will work online
    - now dynamically creating elements

---
+++ _site/js/home.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+    loadTopProjects();
+    
+    $('.nav-element').on('click', function(){
+
+        let nav_to = $(this).id;
+
+        console.log(nav_to);
+        console.log('Clicked on a nav element');
+
+    });
+
+});
+
+function loadTopProjects() {
+
+    console.log('trying to do the thing!');
+
+    loadProject(
+        'assets/projects/chaos/chaos-gameplay-square.png',
+        'Cutting Up Chaos In-Game Screenshot',
+        'Cutting Up Chaos was a rogue-like deck builder in players were tasked with defeating enemies through any means! Even cutting up their own cards and attching them to others to win!'
+    );
+
+}
+
+function loadProject(img, img_alt, text) {
+
+    let html = '';
+
+    html += '<div class="single-project">';
+    html += '<img src="' + img + '" alt="' + img_alt + '">';
+    html += '<p>' + text + '</p>';
+    html += '</div>';
+
+    $('#top-project-list').append(html);
+
+}
---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    
+    
+
+});
+
+// Can select ids by...
+// $('#id-tag').~
+
+// Can select class by...
+// $('.class-name').~
+
+// How to find
+// $('#container class').find('some id or class').children()
